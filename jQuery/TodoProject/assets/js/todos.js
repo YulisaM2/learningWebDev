ab9a429bc1e926ef504cb34e65facc291b2dd65f
ym2 <a01570056@itesm.mx>
2020-06-29 17:31:36 -0400

    Pataptap Clone JQuery, JS and CSS Practice
    WIP

--- jQuery/TodoProject/assets/js/todos.js
+++ jQuery/TodoProject/assets/js/todos.js
@@ -1,5 +1,8 @@
 // Strike element when selected
-$("li").click(function(){
+// Using on function because new elements could be created later and we want to add listeners to them too 
+// By using on with the parent element(which is created during 1st instance) we can do this
+// This JQuery method means add a click listener to the li elements inside the ul 
+$("ul").on("click", "li", function(){
    
     // Method 1. Manual logic 
     // // Color returns rgb string, so we must compare it to gray's rbg code
@@ -20,7 +23,8 @@ $("li").click(function(){
 });
 
 // Deleting element when clicking on X
-$("span").click(function(event){
+// This JQuery method means add click listener to spans inside the ul
+$("ul").on("click", "span", function(event){
 
     // To remove li (element enclosing the span/parent) 
     $(this).parent().fadeOut(500, function(){
@@ -28,4 +32,18 @@ $("span").click(function(event){
     })
     // Used to avoid event bubbling (that parents' events are triggered as well)
     event.stopPropagation();
+});
+
+// Adding new element to list
+$("input[type='text']").keypress(function(event){
+    // If enter is pressed, add element
+    if (event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
 });
